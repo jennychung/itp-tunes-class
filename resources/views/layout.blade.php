7cb2282f228389f12d87cfa632b018a413d680a9
Jenny Chung <jennychung1@Jenny-Chung.T-mobile.com>
2019-03-17 19:00:33 -0700

    updated bootstrap

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <title>@yield('title')</title>
   <!-- yay! -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
 <body style="margin:10%">
 
